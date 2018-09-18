6b5102cfea93328c270f441b52cc0e30273f3ab3
ibrahem <ibrahemid@gmail.com>
2018-09-18 12:49:51 +0800

    done

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,16 +12,23 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
+    String API_URL=BASE_URL+"";
+
 
     // Member Variables:
     TextView mPriceTextView;
@@ -35,8 +42,7 @@ public class MainActivity extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
 
         // Create an ArrayAdapter using the String array and a spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.currency_array, R.layout.spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.currency_array, R.layout.spinner_item);
 
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
@@ -46,10 +52,54 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Set an OnItemSelected listener on the spinner
 
+
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("bitcoin", "onItemSelected: "+parent.getItemAtPosition(position));
+                letsDoSomeNetworking(parent.getItemAtPosition(position).toString());
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("bitcoin", "onNothingSelected: nothing is selected");
+
+            }
+        });
     }
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
+        StringBuilder sb1 = new
+                StringBuilder(BASE_URL);
+        sb1.append(url);
+        Log.d("bitcoin", "letsDoSomeNetworking: url sb1 is  "+sb1.toString());
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(sb1.toString(), new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject responseBody) {
+                Log.d("a", "onSuccess: ");
+                try {
+                    String price = responseBody.getString("last");
+                    mPriceTextView.setText(price);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+
+
+
+
+
+
+
 
 //        AsyncHttpClient client = new AsyncHttpClient();
 //        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
